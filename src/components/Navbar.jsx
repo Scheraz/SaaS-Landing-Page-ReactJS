d86e61a2736219cce702d7e891ded48efcb49e99
Scheraz <sherazkhanpk.sk@gmail.com>
2025-11-04 23:14:27 +0800

    2 sections remaining

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import { Menu, X } from 'lucide-react'
+import { useState } from 'react'
+
+const Navbar = () => {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
+  return (
+    <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
+      <div className="max-w-7xl mx-auto lg:px-8 sm:px-6">
+        <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+          <div className="flex items-center space-x-1 group cursor-pointer">
+            <div>
+              <img
+                src="/logo.png"
+                alt="CodeFlow"
+                className="size-6 sm:size-8"
+              />
+            </div>
+            <span className="text-lg sm:text-xl md:text-2xl font-medium">
+              <span className="text-white">Code</span>
+              <span className="text-blue-400">Flow</span>
+            </span>
+          </div>
+
+          {/* {NavLinks} */}
+          <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+            <a
+              href="#features"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Testimonials
+            </a>
+          </div>
+          <button
+            className="md:hidden p-2 text-gray-300 hover:text-white cursor-pointer"
+            onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+          >
+            {mobileMenuIsOpen ? (
+              <X className="size-5 sm:size-6" />
+            ) : (
+              <Menu className="size-5 sm:size-6" />
+            )}
+          </button>
+        </div>
+      </div>
+      {mobileMenuIsOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-top border-slate-800 animate-in slide-in-from-top duration-300">
+          <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+            <a
+              href="#features"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Testimonials
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
+  )
+}
+export default Navbar
